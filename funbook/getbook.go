495977d1e03a4a0f2f4361607e7a93242d733aba
seth127 <seth127@gmail.com>
2020-07-12 16:51:46 -0400

    starting to clean things up

--- funbook/getbook.go
+++ funbook/getbook.go
@@ -3,56 +3,48 @@ package main
 import (
 	"bufio"
 	"log"
+	"time"
 
-	//"strings"
+	"strings"
 
 	//"io"
 	//"os"
 
 	"net/http"
 	"fmt"
+
+	"math/rand"
 )
 
 func main() {
+	// pick random line
+	maxLines := 10000
+	rand.Seed(time.Now().UnixNano())
+	pick := rand.Intn(maxLines)
+	fmt.Println(pick)
+
 	// Make HTTP GET request
-	response, err := http.Get("https://www.devdungeon.com/")
+	response, err := http.Get("https://www.gutenberg.org/files/2701/2701-h/2701-h.htm")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer response.Body.Close()
 
-	// Copy data from the response to standard output
-	//n, err := io.Copy(os.Stdout, response.Body)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//buf := make([]byte, 8192)
-	//for i := 1; i <= 10; i++ {
-	//	n, error := response.Body.Read(buf)
-	//
-	//	if error == io.EOF {
-	//		break
-	//	} else if error != nil {
-	//		log.Fatal(error)
-	//	}
-	//
-	//	fmt.Printf("%d\ttype: %T\tthing: %v\t%v\n", i, n, n, string(buf[:n])[:10])
-	//
-	//}
-
 	scanner := bufio.NewScanner(response.Body)
 	scanner.Split(bufio.ScanLines) // Set up the split function.
 
-	//buf := make([]byte, 8192)
-	i := 0
+	pc := 0
 	for scanner.Scan() {
-		i++
+
 		t := scanner.Text()
 
-		fmt.Printf("%d\ttype: %T\tthing: %v\n", i, t, t)
+		if strings.Contains(t, "<p>") {
+			pc++
+		}
 
-		if i > 10 {
+		if pc == pick {
+			fmt.Printf("%d\t%v\n", pc, t)
+		} else if pc > pick {
 			break
 		}
 	}
